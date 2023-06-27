b6f926ba18146a3fb7e6b2ad54c329fb58eee0aa
Aasneh Prasad <aasnehprasad@gmail.com>
2023-06-27 18:13:37 +0530

    updated

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
 
   // fetch response from server
 
-  const response=await fetch ('http://localhost:5000',{
+  const response=await fetch ('https://chatex.onrender.com',{
     method:'POST',
     headers:{
       'Content-Type':'application/json'
